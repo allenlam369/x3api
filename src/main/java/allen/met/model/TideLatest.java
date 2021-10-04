40c98eadf807e39f85e539aabc5310decef77ebf
allen <coffee.n.tea@gmail.com>
2021-10-04 09:14:44 +0800

    morning update

--- src/main/java/allen/met/model/TideLatest.java
+++ src/main/java/allen/met/model/TideLatest.java
@@ -14,9 +14,6 @@ public class TideLatest implements Serializable {
 	private static final long serialVersionUID = -8099607682373626452L;
 
 	@Id
-	@Column(name = "id", nullable = false)
-	int id;
-
 	@Column(name = "station_id", nullable = false)
 	int station_id;
 
@@ -28,14 +25,6 @@ public class TideLatest implements Serializable {
 
 	@Column(name = "name")
 	String name;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public int getStation_id() {
 		return station_id;
@@ -73,7 +62,7 @@ public class TideLatest implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + id;
+		result = prime * result + station_id;
 		return result;
 	}
 
@@ -86,7 +75,7 @@ public class TideLatest implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		TideLatest other = (TideLatest) obj;
-		if (id != other.id)
+		if (station_id != other.station_id)
 			return false;
 		return true;
 	}
